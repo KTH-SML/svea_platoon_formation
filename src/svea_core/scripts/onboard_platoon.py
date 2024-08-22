0af9618e748151623a9597fb07dcde3a3da8677f
xiao3913 <xiao2@kth.se>
2024-08-22 09:35:21 +0200

    add mocap and nats and debug onboard.py

--- src/svea_core/scripts/onboard_platoon.py
+++ src/svea_core/scripts/onboard_platoon.py
@@ -8,8 +8,9 @@ from scipy.signal import savgol_filter
 
 import rospy
 from std_msgs.msg import Float32MultiArray as FloatArray
+from nav_msgs.msg import Odometry
 from geometry_msgs.msg import PoseWithCovarianceStamped
-from tf.transformations import quaternion_from_euler, euler_from_quaterion
+from tf.transformations import quaternion_from_euler, euler_from_quaternion
 
 
 from svea.states import VehicleState
@@ -23,6 +24,10 @@ from cv2 import aruco
 from cv_bridge import CvBridge
 import message_filters as mf
 from sensor_msgs.msg import Image, CameraInfo
+
+from nav_msgs.msg import Path
+from svea.simulators.viz_utils import publish_path
+from geometry_msgs.msg import Pose, PoseStamped, PoseArray
 
 
 def load_param(name, value=None):
@@ -59,6 +64,34 @@ def publish_initialpose(state, n=10):
         pub.publish(p)
         rate.sleep()
 
+def lists_to_pose_stampeds(x_list, y_list, yaw_list=None, t_list=None):
+    poses = []
+    for i in range(len(x_list)):
+        x = x_list[i]
+        y = y_list[i]
+
+        curr_pose = PoseStamped()
+        curr_pose.header.frame_id = 'mocap'
+        curr_pose.pose.position.x = x
+        curr_pose.pose.position.y = y
+
+        if not yaw_list is None:
+            yaw = yaw_list[i]
+            quat = tf.transformations.quaternion_from_euler(0.0, 0.0, yaw)
+            curr_pose.pose.orientation.x = quat[0]
+            curr_pose.pose.orientation.y = quat[1]
+            curr_pose.pose.orientation.z = quat[2]
+            curr_pose.pose.orientation.w = quat[3]
+
+        if not t_list is None:
+            t = t_list[i]
+            curr_pose.header.stamp = rospy.Time(secs = t)
+        else:
+            curr_pose.header.stamp = rospy.Time.now()
+
+        poses.append(curr_pose)
+    return poses
+
 class svea_platoon:
 
     def __init__(self):
@@ -67,26 +100,20 @@ class svea_platoon:
         rospy.init_node("svea_platoon")
 
         ## Generate reference path
-        wp_x = [1.5, -1.5]
-        wp_y = [-2.5, 2.5]
+        #wp_x = [1.5, -1.5]
+        #wp_y = [-2.5, 2.5]
+        
+        #wp_x = [1.5, 1.2, 0.2, -0.57]
+        #wp_y = [-2.5, 0.4,0.8,1.1]
+
+        wp_x = [1.1, 0.8, 0.4, -0.3, -0.9]
+        wp_y = [-1.7, 0,0.85,1.05,1.1]
         self.ref_path = CubicSpline2D(wp_x, wp_y)   
 
-
-        ## Parameters for Aruco
-        self.SUB_IMAGE = load_param("~sub_image")
-        self.SUB_CAMERA_INFO = replace_base(self.SUB_IMAGE, "camera_info")
-        self.ARUCO_DICT_NAME = load_param("~aruco_dict", "DICT_4X4_250")
-        self.ARUCO_SIZE = load_param("~aruco_size", "0.05")
-        self.ARUCO_TF_NAME = load_param("~aruco_tf_name", "aruco")
-        self.PUB_ARUCO_POSE = load_param("~pub_aruco_pose", "aruco_pose")
-        ## Aruco
-        self.aruco_size = float(self.ARUCO_SIZE)
-        dict_name = getattr(aruco, self.ARUCO_DICT_NAME)
-        self.aruco_dict = aruco.Dictionary_get(dict_name)     
-        self.bridge = CvBridge()   
+        wp_x, wp_y,_,_,_ = calc_spline_course(wp_x, wp_y, ds=0.2)
 
         ## Parameters for SVEA
-        self.USE_RVIZ = load_param("~use_rvis", False)
+        self.USE_RVIS = load_param("~use_rvis", False)
         self.STATE = load_param("~state", [0, 0, 0, 0])
         
         name_dict = {"svea0":0, "svea1":1, "svea2":2, "svea3":3, "svea4":4, "svea5":5}
@@ -118,6 +145,17 @@ class svea_platoon:
         self.svea = SVEAManager(LocalizationInterface, PurePursuitController, data_handler = RVIZPathHandler if self.USE_RVIS else TrajDataHandler)
         self.svea.start(wait = True)
 
+        self.svea.data_handler.update_traj(wp_x,wp_y)
+        path_topic = "path_plan"
+        self.path_plan_pub = \
+            rospy.Publisher(path_topic, Path, queue_size=1, latch=True)
+        path = Path()
+        path.header.stamp = rospy.Time.now()
+        path.header.frame_id = 'mocap'
+
+        path.poses = lists_to_pose_stampeds(wp_x, wp_y, None, None)
+        self.path_plan_pub.publish(path)
+        
         self.control_dt = 0.1
 
 
@@ -135,13 +173,13 @@ class svea_platoon:
         self.ds_sample = []
         self.t_sample = []
         self.opflow_leader = 0
-        self.desired_velocity = 1.0  #set the desired platoon velocity
+        self.desired_velocity = 0.4  #set the desired platoon velocity
         self.desired_dist = 0.6      #set the desired platoon distance
         self.k1 = 1
         self.k2 = 1
         self.k3 = 1
-        self.k4 = 1
-        self.k5 = 1
+        self.k4 = 4
+        self.k5 = 5
         self.k6 = 1
 
         self.L = 0.324               #wheel base length
@@ -152,6 +190,9 @@ class svea_platoon:
 
         self.target_velocity = self.desired_velocity
         self.target_steering = 0 #  kappa = tan(delta)/L
+
+        self.control_counter = 0
+        self.state_counter = 0
 
         self.create_control_publisher()
         self.create_state_publisher()
@@ -188,79 +229,101 @@ class svea_platoon:
         self.svea.visualize_data()
 
     def get_control(self):
-        tild_y = self.y_path
-        tild_theta = self.yaw_path
-        v = self.v
-        kappa = self.kappa
-        dkappa = self.kappa
-        tild_v = v - self.desired_velocity
+        if self.state_counter == 1:
+            tild_y = self.y_path
+            tild_theta = self.yaw_path
+            v = self.v
+            kappa = self.kappa
+            dkappa = self.kappa
+            tild_v = v - self.desired_velocity
 
-        #add case for vehicle 1!!
-        if self.leader_vehicle["vehicle_name"] == "svea0":   
-            current_time = rospy.get_time()
-            delta_time = current_time - self.state_timer
-            self.state_timer = current_time
-
-            self.leader_vehicle["v"] = self.desired_velocity
-            self.leader_vehicle["v_path"] = self.desired_velocity
-            self.leader_vehicle["acc"] = 0.0
-            leader_s = self.leader_vehicle["s_path"]
-            self.leader_vehicle["s_path"] = self.leader_vehicle["s_path"] + self.leader_vehicle["v_path"] * delta_time
-
-
-        tild_s = self.leader_vehicle["s"] - self.s_path
-        tild_nu = self.leader_vehicle["v_path"] - self.v_path
-        
-        if tild_y >= self.road_width_left - self.road_width/2:
-            opflow_wall = self.opflow_wall_left
-        else:
-            opflow_wall = self.opflow_wall_right
-        
-        if tild_theta == 0:
-            tild_theta = 1e-9
-                
-        tild_e = tild_s - self.desired_dist
-        
-        vehicle_kappa = -self.k1*tild_y*np.sin(tild_theta)/tild_theta - self.k2*np.sign(v)*tild_theta - self.k3*np.sign(v)*opflow_wall + kappa*np.cos(tild_theta)/(1-kappa*tild_y)
-        
-        acc = self.leader_vehicle["acc"] + self.k4*tild_e + self.k5*tild_nu + self.k6*self.opflow_leader
-        dtild_theta = v*(vehicle_kappa - kappa*np.cos(tild_theta)/(1-kappa*tild_y))
-        vehicle_acc = 1/np.cos(tild_theta)*(acc*(1-kappa*tild_y))+v*np.sin(tild_theta)*dtild_theta-self.v_path*(dkappa*self.v_path*tild_y+kappa*v*np.sin(tild_theta))
-        
             
-        self.target_velocity = v + self.control_dt * vehicle_acc 
-        self.target_steering = np.arctan2(vehicle_kappa, self.L) #  kappa = tan(delta)/L
+            if self.control_counter == 0:
+                #add case for vehicle 1!!
+                if self.leader_vehicle["vehicle_name"] == "svea0":   
+                    current_time = rospy.get_time()
+                    delta_time = current_time - self.state_timer
+                    self.state_timer = current_time
 
-        current_time = rospy.get_time()
-        delta_time = current_time - self.pid_timer
-        self.pid_timer = current_time
+                    self.leader_vehicle["v"] = self.desired_velocity
+                    self.leader_vehicle["v_path"] = self.desired_velocity
+                    self.leader_vehicle["acc"] = 0.0
+                    self.leader_vehicle["s_path"] = self.s_path + 1
+                    self.control_counter = 1
+            else:
+                #add case for vehicle 1!!
+                if self.leader_vehicle["vehicle_name"] == "svea0":   
+                    current_time = rospy.get_time()
+                    delta_time = current_time - self.state_timer
+                    self.state_timer = current_time
 
-        error = self.target_velocity - v
-        self.error_sum += error * delta_time
-        P = error * self.K_p
+                    self.leader_vehicle["v"] = self.desired_velocity
+                    self.leader_vehicle["v_path"] = self.desired_velocity
+                    self.leader_vehicle["acc"] = 0.0
+                    self.leader_vehicle["s_path"] = self.leader_vehicle["s_path"] + self.leader_vehicle["v_path"] * delta_time
 
-        if self.error_sum > self.max_error_sum:
-            self.error_sum = self.max_error_sum
-        elif self.error_sum < - self.max_error_sum:
-            self.error_sum = - self.max_error_sum        
 
-        I = self.error_sum * self.K_i
-        correction = P + I
-        speed_val = self.target_velocity + correction
-        if speed_val <= 0:
-            speed_val = 0
-        
-        #send control to low level
-        #self.svea.send_control(self.target_steering, speed_val)
 
-        control_input = [acc, vehicle_kappa]  #wrap the local state into a FloatArray
-        control_input_msg = FloatArray(data=control_input)
-        self.control_publisher.publish(control_input_msg)
+            tild_s = self.leader_vehicle["s_path"] - self.s_path
+            tild_nu = self.leader_vehicle["v_path"] - self.v_path
+            
+            rospy.loginfo('-----')
+            #rospy.loginfo(self.leader_vehicle["s_path"])
+            #rospy.loginfo(self.s_path)
+            #rospy.loginfo(tild_s - self.desired_dist)
+            #rospy.loginfo(tild_nu)
+            #rospy.loginfo(tild_y)
+            #rospy.loginfo(tild_theta)
+            if tild_y >= self.road_width_left - self.road_width/2:
+                opflow_wall = self.opflow_wall_left
+            else:
+                opflow_wall = self.opflow_wall_right
+            
+            if tild_theta == 0:
+                tild_theta = 1e-9
+                    
+            tild_e = tild_s - self.desired_dist
+            
+            vehicle_kappa = -self.k1*tild_y*np.sin(tild_theta)/tild_theta - self.k2*np.sign(v)*tild_theta - self.k3*np.sign(v)*opflow_wall + kappa*np.cos(tild_theta)/(1-kappa*tild_y)
+            
+            acc = self.leader_vehicle["acc"] + self.k4*tild_e + self.k5*tild_nu + self.k6*self.opflow_leader
+            dtild_theta = v*(vehicle_kappa - kappa*np.cos(tild_theta)/(1-kappa*tild_y))
+            vehicle_acc = 1/np.cos(tild_theta)*(acc*(1-kappa*tild_y))+v*np.sin(tild_theta)*dtild_theta-self.v_path*(dkappa*self.v_path*tild_y+kappa*v*np.sin(tild_theta))
+            
+                
+            self.target_velocity = v + self.control_dt * vehicle_acc 
+            self.target_steering = np.arctan2(vehicle_kappa, self.L) #  kappa = tan(delta)/L
 
-        #debug = [tild_e, tild_nu, self.v_path, vehicle_acc, self.target_velocity, v, self.leader_vehicle["s_path"] - self.s_path, tild_y*np.sin(tild_theta)/tild_theta, self.target_steering, vehicle_kappa, np.sign(v)*tild_theta]  #wrap the local state into a FloatArray
-        debug = [v, self.v_path, tild_v, tild_s, tild_e, self.leader_vehicle["s_path"], self.s_path, acc, vehicle_acc, self.target_velocity]
-        debug_msg = FloatArray(data=debug)
-        self.debug_publisher.publish(debug_msg)  
+            current_time = rospy.get_time()
+            delta_time = current_time - self.pid_timer
+            self.pid_timer = current_time
+
+            error = self.target_velocity - v
+            self.error_sum += error * delta_time
+            P = error * self.K_p
+
+            if self.error_sum > self.max_error_sum:
+                self.error_sum = self.max_error_sum
+            elif self.error_sum < - self.max_error_sum:
+                self.error_sum = - self.max_error_sum        
+
+            I = self.error_sum * self.K_i
+            correction = P + I
+            speed_val = self.target_velocity + correction
+            if speed_val <= 0:
+                speed_val = 0
+            
+            #send control to low level
+            self.svea.send_control(self.target_steering, speed_val)
+
+            control_input = [acc, vehicle_kappa]  #wrap the local state into a FloatArray
+            control_input_msg = FloatArray(data=control_input)
+            self.control_publisher.publish(control_input_msg)
+
+            #debug = [tild_e, tild_nu, self.v_path, vehicle_acc, self.target_velocity, v, self.leader_vehicle["s_path"] - self.s_path, tild_y*np.sin(tild_theta)/tild_theta, self.target_steering, vehicle_kappa, np.sign(v)*tild_theta]  #wrap the local state into a FloatArray
+            debug = [v, self.v_path, tild_nu, tild_s, tild_e, self.leader_vehicle["s_path"], self.s_path, acc, vehicle_acc, self.target_velocity]
+            debug_msg = FloatArray(data=debug)
+            self.debug_publisher.publish(debug_msg)  
 
     def create_subscription_to_leader(self):
         topic_name = "/" + self.leader_vehicle["vehicle_name"] + "/local_state"
@@ -299,14 +362,15 @@ class svea_platoon:
         self.debug_publisher = rospy.Publisher(topic_name, FloatArray, queue_size = 1, tcp_nodelay = True) 
 
     def create_subscription_to_state(self):
-        topic_name = "/qualisys/" + self.leader_vehicle["vehicle_name"] + "/odom"
-        rospy.Subscriber(topic_name, FloatArray, self.update_vehicle_state_mocap, tcp_nodelay = True, queue_size = 1)
+        topic_name = "/qualisys/" + self.vehicle_name + "/odom"
+        rospy.Subscriber(topic_name, Odometry, self.update_vehicle_state_mocap, tcp_nodelay = True, queue_size = 1)
 
     def update_vehicle_state_mocap(self, state):
+        self.state_counter = 1
         self.x = state.pose.pose.position.x
         self.y = state.pose.pose.position.y
-        
-        euler = euler_from_quaterion(state.pose.pose.orientation.x, state.pose.pose.orientation.y, state.pose.pose.orientation.z, state.pose.pose.orientation.w)
+        quaternion = (state.pose.pose.orientation.x, state.pose.pose.orientation.y, state.pose.pose.orientation.z, state.pose.pose.orientation.w)
+        euler = euler_from_quaternion(quaternion)
         self.yaw = euler[2]
         self.v = np.sqrt(state.twist.twist.linear.x**2 + state.twist.twist.linear.y**2) 
 
